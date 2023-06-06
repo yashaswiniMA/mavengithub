f1c1c096786c4d7f7c4fb273ce84fbc97e57fff2
Gagan Kumar V S <Gagan Kumar V S@LAPTOP-8MHAELBN>
2023-06-06 11:33:14 +0530

    commit

---
+++ yashumaven12/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,11 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+@Test
+public void testcreateCustomer() {
+	Reporter.log("createCustomer",true);
+}
+}
